f135507e7d0640588f4acf090203b6f027937501
Jack CAI <jcai@turo.com>
2024-09-17 15:36:47 +0200

    appNestedGraph animation

--- app/src/main/java/com/example/quazz/navigation/AppNestedGraph.kt
+++ app/src/main/java/com/example/quazz/navigation/AppNestedGraph.kt
@@ -1,16 +1,22 @@
 package com.example.quazz.navigation
 
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
 import com.example.quazz.QuazzAppState
 import com.example.quazz.app.presentation.create.CreateScreen
+import com.example.quazz.app.presentation.create.quizz.CreateQuizzScreen
 import com.example.quazz.app.presentation.home.HomeScreen
 import com.example.quazz.app.presentation.profile.ProfileScreen
 import com.example.quazz.app.presentation.search.SearchScreen
@@ -35,10 +41,25 @@ fun NavGraphBuilder.appGraph(appState: QuazzAppState){
                     restartApp = { route -> appState.clearAndNavigate(route) })
             }
         }
-        composable(route = Route.CreateRoute.route) {
+        composable(route = Route.CreateListRoute.route,
+            ) {
             ScaffoldBottomApp(navController = appState.navController) {
                 CreateScreen(it, appState.navController)
             }
+        }
+        composable(route = Route.CreateRoute.route,
+            enterTransition = {
+                scaleIn(
+                    animationSpec = spring(stiffness = Spring.StiffnessVeryLow),
+                    transformOrigin = TransformOrigin(0.9f, 0.9f))
+            },
+            exitTransition = { scaleOut(
+                animationSpec = spring(stiffness = Spring.StiffnessVeryLow),
+                transformOrigin = TransformOrigin(0.9f, 0.9f)) }) {
+            CreateQuizzScreen(
+                popUp = { appState.popUp() },
+                restartApp = { route -> appState.clearAndNavigate(route) }
+            )
         }
     }
 }
@@ -47,6 +68,6 @@ sealed class BottomNavItem(val route: String, val icon: ImageVector, val label: 
     data object Home : BottomNavItem(Route.HomeRoute.route, Icons.Default.Home, "Home")
     data object Search : BottomNavItem(Route.SearchRoute.route, Icons.Default.Search, "Search")
     data object Profile : BottomNavItem(Route.ProfileRoute.route, Icons.Default.Person, "Profile")
-    data object Create : BottomNavItem(Route.CreateRoute.route, Icons.Default.AddCircle, "Create")
+    data object Create : BottomNavItem(Route.CreateListRoute.route, Icons.Default.AddCircle, "Create")
 
 }
